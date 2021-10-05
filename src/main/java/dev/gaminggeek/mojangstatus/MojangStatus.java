2066968dc18fe1c2efa776b82960b63dae238153
ChachyDev <shalomadecoolboy@outlook.com>
2021-10-05 17:41:46 +0100

    Update version

--- src/main/java/dev/gaminggeek/mojangstatus/MojangStatus.java
+++ src/main/java/dev/gaminggeek/mojangstatus/MojangStatus.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(name = "MojangStatus", modid = MojangStatus.MODID, version = MojangStatus.VERSION)
 public class MojangStatus {
     public static final String MODID = "mojang_status";
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "2.0.0";
     private static final StatusCheck statusCheck = new StatusCheck();
     private static StatusConfig statusConfig;
 
